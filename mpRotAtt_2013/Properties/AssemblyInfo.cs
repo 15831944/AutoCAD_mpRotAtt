95eba69ac7a24f09b75178d6efeb8971fd599bdb
Modis <cjmodis@gmail.com>
2018-06-12 20:55:03 +0300

    Remove 2010-2012. Change icons

--- mpRotAtt_2013/Properties/AssemblyInfo.cs
+++ mpRotAtt_2013/Properties/AssemblyInfo.cs
@@ -5,22 +5,25 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
+
 [assembly: AssemblyTitle("mpRotAtt")]
 [assembly: AssemblyDescription("2013")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ModPlus")]
+[assembly: AssemblyCompany("modplus.org")]
 [assembly: AssemblyProduct("ModPlus")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright © ModPlus")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
 // для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
 // COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("55d5f451-1e4f-4bf9-9009-7106c3d31729")]
+
+[assembly: Guid("a87fd4f2-453d-40fb-9686-a6c7e2e9065d")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
@@ -32,5 +35,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
